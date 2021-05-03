6219061abe916cc0324e867b3a11b62e65f6f5e8
August <augustraro112@hotmail.com>
2021-05-03 12:08:48 +0200

    Added more styling.

--- api/routes/favoritesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const favoritesController = require("../controllers/favoritesController");
-
-router.post("/save-favorite", favoritesController.saveFavorite);
-
-module.exports = router;
